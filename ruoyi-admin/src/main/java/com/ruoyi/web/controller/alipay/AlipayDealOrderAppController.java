5b076c0b7e0ea87cfa985aeea330601411099a72
jess.unity@skig.tech <jess.unity@skig.tech>
2023-10-10 21:59:04 +0800

    修改回调状态与产品过滤

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/alipay/AlipayDealOrderAppController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/alipay/AlipayDealOrderAppController.java
@@ -68,7 +68,6 @@ public class AlipayDealOrderAppController extends BaseController {
         //查询产品类型下拉菜单
         AlipayProductEntity alipayProductEntity = new AlipayProductEntity();
         alipayProductEntity.setStatus(1);
-        alipayProductEntity.setProductCode("2");
         List<AlipayProductEntity> list = iAlipayProductService.selectAlipayProductList(alipayProductEntity);
         mmap.put("productList", list);
         return prefix + "/orderApp";

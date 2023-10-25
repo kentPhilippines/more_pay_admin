fde2e7c1669452dd6cfaa2b3a7328b18d3fd7bd0
jess.unity@skig.tech <jess.unity@skig.tech>
2023-10-26 00:55:51 +0800

    绑定渠道限制

--- ruoyi-system/src/main/java/com/ruoyi/alipay/domain/AlipayChanelFee.java
+++ ruoyi-system/src/main/java/com/ruoyi/alipay/domain/AlipayChanelFee.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 /**
@@ -27,4 +28,6 @@ public class AlipayChanelFee extends BaseEntity {
     private Date submitTime;
     private String rotation;
     private Integer status;
+    private BigDecimal minAmount;
+    private BigDecimal maxAmount;
 }
